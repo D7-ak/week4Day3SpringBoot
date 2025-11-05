88662463e6718a5f4320544a476072273506f3ae
ABDULRAHMAN AYIDH ALSHAHRANI <139352406+D7-ak@users.noreply.github.com>
2025-11-05 14:12:06 +0300

    Add REST endpoints for name, age, and health checks

--- src/main/java/com/example/week4day3springboot/Controller.java
+++ src/main/java/com/example/week4day3springboot/Controller.java
@@ -1,4 +1,32 @@
 package com.example.week4day3springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/name")
+    public  String myName(){
+        return "My name is abdulrahman";
+    }
+    @GetMapping("/age")
+    public String age(){
+        return "my age27";
+    }
+    @GetMapping("/check/status")
+    public String check(){
+        return "every thing is ok";
+    }
+    @GetMapping("/health")
+    public String helth(){
+        return "the server is up and running";
+    }
+    @GetMapping("/names")
+    public String[] names(){
+        String[] names = {"ali","ahmad"};
+        return names;
+    }
+
+
 }
